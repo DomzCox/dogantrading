225033c9916d9282c4c88bcd6cd177407993c1b4
unknown <nyron.biz@gmail.com>
2024-10-15 17:42:04 -0500

    build with content on home page

--- src/shared/components/navigation/Topnav.jsx
+++ src/shared/components/navigation/Topnav.jsx
@@ -6,27 +6,27 @@ export default function Topnav() {
     
     <div className="container flex flex-row">
     <h1 className="text-4xl mr-20 text-teal-900 font-bold pt-2 mt-3">Telach-Trading</h1>
-      <div className="m-3 p-3 font-semibold ">
+      <div className="m-3 p-3 font-semibold underline">
         <p >
           Home
         </p>
       </div>
 
-      <div className="m-3 p-3 font-semibold ">
+      <div className="m-3 p-3 font-semibold underline">
         <p>
           About
         </p>
       </div>
 
-      <div className="m-3 p-3 font-semibold ">
+      <div className="m-3 p-3 font-semibold underline">
         <p>
           Contact
         </p>
       </div>
 
-      <div className="m-3 p-3 font-semibold ">
+      <div className="m-3 p-3 font-semibold underline">
         <p>
-          Products
+          Shop
         </p>
       </div>
 
