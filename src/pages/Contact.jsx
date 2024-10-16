dc85c6300dd565af16ff5b1ad61e191c44f6d62d
unknown <nyron.biz@gmail.com>
2024-10-15 20:08:17 -0500

    added all the contact information to the form will style later

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,8 +3,29 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div className="container">
-        <h1>Contact page</h1>
+    <div className="container text-center">
+              <h1 className="mt-10 text-2xl">Contact us</h1>
+
+          <div className=''>
+            <label htmlFor="name">Name: </label>
+            <input className="border-2" type="text" placeholder='Name'/>
+            
+          </div>
+          <div>
+            <label htmlFor="email">Email: </label>
+          <input className="border-2" type="text" placeholder='Name'/>
+            
+          </div>
+          <div>
+            <label htmlFor="message">Your message: </label>
+          <textarea className="border-2" />
+          </div>
+
+          <div>
+            <button className="mt-10">
+              Send Message
+            </button>
+          </div>
     </div>
   )
 }
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 
+
 export default function Shop() {
   return (
     <div className="container">
-        <h1 className="text-2xl">
-            Shop
+        <h1 className="text-2xl pt-10 text-teal-950 py-10 text-center">
+            Trade Shop!
         </h1>
+            <ol className="flex justify-evenly gap-3">
+                <li>
+                    <h2 className="underline">T-Shirts</h2>
+                    <div className="border-solid mt-10">
+                        <img src="https://placehold.co/400" alt="" />
+                    </div>
+                </li>
+                <li>
+                    Mugs
+                    <div className="border-solid mt-10">
+                        <img src="https://placehold.co/400" alt="" />
+                    </div>
+                    </li>
+                <li>Caps
+                <div className="border-solid mt-10">
+                        <img src="https://placehold.co/400" alt="" />
+                    </div>
+                </li>
+            </ol>
+        
     </div>
   )
 }
