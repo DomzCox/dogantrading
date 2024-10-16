8978f1fc5d53f24ecb2d91f09092b0e4466fb811
unknown <nyron.biz@gmail.com>
2024-10-15 19:36:15 -0500

    added babble dependencies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Landing from "./shared/components/Landing";
+
 import Topnav from "./shared/components/navigation/Topnav";
-import Footer from "./shared/components/Footer";
+
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
